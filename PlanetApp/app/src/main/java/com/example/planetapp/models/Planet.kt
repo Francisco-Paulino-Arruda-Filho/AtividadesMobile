3068a56d7a57d62f2fcd4b0c126b919e72bd9bed
Francisco Paulino Arruda Filho <fpaulinofilho04@gmail.com>
2024-11-29 21:07:28 -0300

    PlanetApp

---
+++ PlanetApp/app/src/main/java/com/example/planetapp/models/Planet.kt
@@ -0,0 +1,58 @@
+package com.example.planetapp.models
+
+import com.example.planetapp.R
+
+data class Planet(
+    val id: Int,
+    val name: String,
+    val type: String,
+    val galaxy: String,
+    val distanceFromSun: String,
+    val diameter: String,
+    val characteristics: String,
+    val imageRes: Int,
+    var isFavorite: Boolean = false
+)
+
+val planetList = listOf(
+    Planet(
+        id = 1,
+        name = "Mercury",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "57.9 million km",
+        diameter = "4,879 km",
+        characteristics = "Smallest planet in the Solar System, closest to the Sun, has a rocky surface full of craters.",
+        imageRes = R.drawable.mercurio
+    ),
+    Planet(
+        id = 2,
+        name = "Venus",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "108.2 million km",
+        diameter = "12,104 km",
+        characteristics = "Thick, toxic atmosphere with sulfuric acid clouds, hottest planet due to greenhouse effect.",
+        imageRes = R.drawable.venus
+    ),
+    Planet(
+        id = 3,
+        name = "Earth",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "149.6 million km",
+        diameter = "12,742 km",
+        characteristics = "Supports life, has water and atmosphere.",
+        imageRes = R.drawable.terra
+    ),
+    Planet(
+        id = 4,
+        name = "Mars",
+        type = "Terrestrial",
+        galaxy = "Milky Way",
+        distanceFromSun = "227.9 million km",
+        diameter = "6,779 km",
+        characteristics = "Known as the 'Red Planet' due to iron oxide on its surface, has the largest volcano in the Solar System.",
+        imageRes = R.drawable.marte
+    ),
+)
